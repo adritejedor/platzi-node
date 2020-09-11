efe0cb30776642110df00fe5bfe130be9ef613ba
adrian <adriantejedor96@gmail.com>
2020-09-11 19:43:41 +0200

    añadida conexion a mongodb con mongoatlas y añadida capa de conexion a bbdd

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,10 @@ const app = express();
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies');
 
+
+//body parser middleware
+app.use(express.json());
+
 moviesApi(app);
 
 
